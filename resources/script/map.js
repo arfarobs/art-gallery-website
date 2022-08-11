c0b422e435186ac9d72d326bde3b2ed42fcde971
arfarobs <arthurroberts@mail.com>
2022-08-11 16:48:00 +0800

    Hidden access token

--- resources/script/map.js
+++ resources/script/map.js
@@ -1,4 +1,6 @@
 const map = L.map('map').setView([41.480492405593466, -71.31096111763853], 16);
+const accessToken = config.ACCESS_TOKEN;
+
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -6,7 +8,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiYXJmYXJvYnMiLCJhIjoiY2wxMGhuNnJlMDU0ODNrcnprM2FieDYwZyJ9.shCLpqfyIUhfeZGaWoEghA'
+    accessToken: accessToken
 }).addTo(map);
 
 const myIcon = L.icon({
